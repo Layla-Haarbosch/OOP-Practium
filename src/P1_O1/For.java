dc655ff483ba021c71f70345595693f0bfaeb477
Layla Haarbosch <90128114+Lulunade@users.noreply.github.com>
2023-11-16 14:44:14 +0100

    finish Opdracht 1

---
+++ src/P1_O1/For.java
@@ -0,0 +1,9 @@
+package P1_O1;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/P1_O2/While.java
@@ -0,0 +1,7 @@
+package P1_O2;
+
+public class While {
+    public static void main(String[] args) {
+
+    }
+}
