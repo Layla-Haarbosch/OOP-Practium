6675cd8a3854b4041682cef7b1051e30d0f498fe
Layla <90128114+Lulunade@users.noreply.github.com>
2023-11-17 10:25:14 +0100

    finish Opdracht 4

---
+++ src/P1_O4/SomVanHeleGetallen.java
@@ -0,0 +1,13 @@
+package P1_O4;
+
+public class SomVanHeleGetallen {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 0; i <= 39; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
