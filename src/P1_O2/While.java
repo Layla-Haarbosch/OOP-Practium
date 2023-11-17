5328ee63c0594778b7ef757cb6354dc57f76de0c
Layla Haarbosch <90128114+Lulunade@users.noreply.github.com>
2023-11-17 09:28:30 +0100

    finish Opdracht 2

--- src/P1_O2/While.java
+++ src/P1_O2/While.java
@@ -2,6 +2,11 @@ package P1_O2;
 
 public class While {
     public static void main(String[] args) {
+        int i = 1;
 
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
     }
 }
