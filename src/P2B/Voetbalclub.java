f20f300e88717f09afd73c8c45b6126c01c3e6ce
Layla <90128114+Lulunade@users.noreply.github.com>
2023-11-17 11:27:14 +0100

    finish Practicum 2B

---
+++ src/P2B/Voetbalclub.java
@@ -0,0 +1,42 @@
+package P2B;
+
+public class Voetbalclub
+{
+    String naam;
+    int aantalGewonnen = 0;
+    int aantalGelijk = 0;
+    int aantalVerloren = 0;
+
+    public Voetbalclub(String naam)
+    {
+        this.naam = naam;
+    }
+
+    public void verwerkResultaat(char ch) {
+        if (ch == 'w') {
+            aantalGewonnen = aantalGewonnen + 1;
+        }
+        if (ch == 'g') {
+            aantalGelijk = aantalGelijk + 1;
+        }
+        if (ch == 'v') {
+            aantalVerloren = aantalVerloren + 1;
+        }
+    }
+
+    public int aantalPunten()
+    {
+        return (this.aantalGewonnen * 3) + this.aantalGelijk;
+    }
+
+    public int aantalGespeeld()
+    {
+        return this.aantalGewonnen + this.aantalGelijk + this.aantalVerloren;
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.naam + "\t" + this.aantalGespeeld() + " " + this.aantalGewonnen + " " + this.aantalGelijk + " " + this.aantalVerloren + " " + this.aantalPunten();
+    }
+}
