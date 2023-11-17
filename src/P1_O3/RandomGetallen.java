392947c4f0f164fc9d9ec4e50b28afc6d9289912
Layla <90128114+Lulunade@users.noreply.github.com>
2023-11-17 10:12:01 +0100

    finish Opdracht 3

---
+++ src/P1_O3/RandomGetallen.java
@@ -0,0 +1,12 @@
+package P1_O3;
+
+public class RandomGetallen
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(Math.random());
+        }
+    }
+}
